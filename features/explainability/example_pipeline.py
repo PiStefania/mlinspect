98d0717d7ece8320442c0649e300896081303188
PiStefania <stefania.patsou@hotmail.com>
2023-09-09 19:22:36 +0300

    tweak train test split to be acceptable for current patching and remove shap code to test predict

--- features/explainability/example_pipeline.py
+++ features/explainability/example_pipeline.py
@@ -33,10 +33,11 @@ data = data.merge(complications, on=['age_group'])
 data['label'] = data['complications'] > 1.2 * data['mean_complications']
 data = data[['smoker', 'last_name', 'county', 'num_children', 'race', 'income', 'label']]
 data = data[data['county'].isin(COUNTIES_OF_INTEREST)]
-y = data["label"]
-X = data.drop("label", axis=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-
+train_data, test_data = train_test_split(data)
+y_train = train_data["label"]
+y_test = test_data["label"]
+X_train = train_data.drop("label", axis=1)
+X_test = test_data.drop("label", axis=1)
 
 impute_and_one_hot_encode = Pipeline([
     ('impute', SimpleImputer(strategy='most_frequent')),
@@ -44,17 +45,18 @@ impute_and_one_hot_encode = Pipeline([
 ])
 featurisation = ColumnTransformer(transformers=[
     ("impute_and_one_hot_encode", impute_and_one_hot_encode, ['smoker', 'county', 'race']),
-    #('word2vec', MyW2VTransformer(min_count=2, vector_size=5), ['last_name']),
     ('numeric', StandardScaler(), ['num_children', 'income']),
 ], remainder='drop')
 
 neural_net = KerasClassifier(model=create_model_predict, epochs=10, batch_size=1, verbose=0, loss='binary_crossentropy',)
-X_t_train = featurisation.fit_transform(X_train, y_train)
+X_t_train = featurisation.fit_transform(train_data, y_train)
 X_t_test = featurisation.fit_transform(X_test, y_test)
 neural_net.fit(X_t_train, y_train)
 print("Mean accuracy: {}".format(neural_net.score(X_t_test, y_test)))
 print(f"Predict first 10 samples: {neural_net.predict(X_t_test[:10])}")
 # Introduce explainability
-explainer = shap.KernelExplainer(neural_net.predict, X_t_train, feature_names=featurisation.get_feature_names_out())
-shap_values = explainer.shap_values(X_t_test[:1], nsamples=100)
-shap.force_plot(explainer.expected_value, shap_values, X_t_test[:1])
+# explainer = shap.KernelExplainer(neural_net.predict, X_t_train)
+# shap_values = explainer.shap_values(X_t_test[:1], nsamples=100)
+# print(f"{featurisation.get_feature_names_out()=}")
+# shap.force_plot(explainer.expected_value, shap_values, X_t_test[:1], feature_names=featurisation.get_feature_names_out())
+# shap.summary_plot(shap_values, X_t_test[:1], feature_names=featurisation.get_feature_names_out(), plot_type="bar")
